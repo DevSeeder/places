39c0d0462a1d4729dce255c650a39aa9f874290b
Maick Speck <maicksantos05@hotmail.com>
2022-05-25 09:36:21 -0300

    module tests

---
+++ test/unit/adapter/neighborhoods.module.spec.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../../../src/app.module';
+import { INestApplication } from '@nestjs/common/interfaces/nest-application.interface';
+import { expect } from 'chai';
+import * as request from 'supertest';
+
+describe('NewStageForEmptyMovesInterfaceModule', () => {
+  let application: INestApplication;
+
+  beforeEach(async function () {
+    application = await NestFactory.create(AppModule);
+    application.init();
+  });
+
+  afterEach(async function () {
+    application.close();
+  });
+
+  describe('#endpoint /', function () {
+    describe('GET /', function () {
+      it('should verify if default service returns 404', function (done) {
+        request(application.getHttpServer())
+          .get('/')
+          .end(function (err, res) {
+            expect(res.status).to.be.equal(404);
+            done(err);
+          });
+      });
+    });
+  });
+});
