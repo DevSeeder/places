1c1891e9736e01d9a045f1fadc63bb61786cc87a
Maick Speck <maicksantos05@hotmail.com>
2022-05-30 13:05:33 -0300

    improve cov

--- src/core/error-handling/filter/error-exception.filter.ts
+++ src/core/error-handling/filter/error-exception.filter.ts
@@ -1,6 +1,7 @@
 import { Catch, HttpException, HttpStatus } from '@nestjs/common';
 import { AbstractExceptionFilter } from './abstract-exception.filter';
 import { CustomExceptionReponse } from '../interface/custom-exception-response.interface';
+import { CustomErrorException } from '../exception/custom-error.exception';
 
 @Catch()
 export class ErrorExceptionFilter extends AbstractExceptionFilter<Error> {
@@ -9,7 +10,7 @@ export class ErrorExceptionFilter extends AbstractExceptionFilter<Error> {
       status: this.getStatus(exception),
       message: exception.message,
       type: exception.name,
-      errorCode: this.getStatus(exception)
+      errorCode: this.getErrCode(exception)
     };
   }
 
@@ -19,5 +20,15 @@ export class ErrorExceptionFilter extends AbstractExceptionFilter<Error> {
     if (exception instanceof HttpException) status = exception.getStatus();
 
     return status;
+  }
+
+  getErrCode(exception: any): number {
+    let errCode = -1;
+
+    if (exception instanceof HttpException) errCode = exception.getStatus();
+
+    if (exception instanceof CustomErrorException) errCode = exception.errCode;
+
+    return errCode;
   }
 }
--- test/unit/core/error-handling/filter/custom-error-exception.filter.spec.ts
+++ test/unit/core/error-handling/filter/custom-error-exception.filter.spec.ts
@@ -2,13 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ExtensionsModule } from '../../../../../src/microservice/adapter/helper/extensions/exensions.module';
 import { expect } from 'chai';
 import { FiltersModule } from '../../../../../src/core/error-handling/filters.module';
-import { HttpStatus } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { CustomErrorExceptionFilter } from '../../../../../src/core/error-handling/filter/custom-error-exception.filter';
 import { EmptyPropException } from '../../../../../src/core/error-handling/exception/empty-prop.exception';
+import { createMock } from '@golevelup/ts-jest';
+import { ExecutionContext } from '@nestjs/common';
+import * as sinon from 'sinon';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../../../../../src/app.module';
 
 describe('CustomErrorExceptionFilter', () => {
   let sut: CustomErrorExceptionFilter;
   let app: TestingModule;
+  let server: INestApplication;
 
   beforeEach(async () => {
     app = await Test.createTestingModule({
@@ -18,10 +24,13 @@ describe('CustomErrorExceptionFilter', () => {
     }).compile();
 
     sut = app.get<CustomErrorExceptionFilter>(CustomErrorExceptionFilter);
+    server = await NestFactory.create(AppModule);
+    await server.init();
   });
 
   afterEach(async () => {
     await app.close();
+    await server.close();
   });
 
   describe('makeCustomResponse', () => {
@@ -35,6 +44,72 @@ describe('CustomErrorExceptionFilter', () => {
       };
       const actual = await sut.makeCustomResponse(mockException);
       expect(JSON.stringify(actual)).to.be.equal(JSON.stringify(mockResponse));
+    });
+  });
+
+  describe('getResponse', () => {
+    it('should call getResponse and call the functions correctly', async () => {
+      const mockResponse = {
+        status: HttpStatus.NOT_ACCEPTABLE,
+        message: `The property 'Any_prop' cannot be empty`,
+        type: 'EmptyPropException',
+        errorCode: 2
+      };
+
+      const mockArgHostResponse = createMock<ExecutionContext>({
+        switchToHttp: () => ({
+          getResponse: () => mockResponse
+        })
+      });
+
+      const mockException = new EmptyPropException('any_prop');
+
+      const actual = await sut.getResponse(mockArgHostResponse, mockException);
+      expect(JSON.stringify(actual)).to.be.equal(JSON.stringify(mockResponse));
+    });
+  });
+
+  describe('catch', () => {
+    it('should call catch and call the functions correctly', async () => {
+      const mockException = new EmptyPropException('any_prop');
+
+      const mockResponse = {
+        status: HttpStatus.NOT_ACCEPTABLE,
+        message: `The property 'Any_prop' cannot be empty`,
+        type: 'EmptyPropException',
+        errorCode: 2
+      };
+
+      const mockAdapterResponse = {
+        status: () => HttpStatus.NOT_ACCEPTABLE,
+        message: `The property 'Any_prop' cannot be empty`,
+        type: 'EmptyPropException',
+        errorCode: 2,
+        json: () => mockResponse
+      };
+
+      const mockArgHostResponse = createMock<ExecutionContext>({
+        switchToHttp: () => ({
+          getResponse: () => mockAdapterResponse
+        })
+      });
+
+      const getResponseSpy = sinon.spy(sut, 'getResponse');
+      const makeCustomResponseSpy = sinon.spy(sut, 'makeCustomResponse');
+      sinon.stub(sut, 'httpAdapter').value(server.getHttpAdapter());
+
+      await sut.catch(mockException, mockArgHostResponse);
+
+      sinon.assert.calledOnceWithExactly(
+        getResponseSpy,
+        mockArgHostResponse,
+        mockException
+      );
+
+      sinon.assert.calledOnceWithExactly(makeCustomResponseSpy, mockException);
+
+      getResponseSpy.restore();
+      makeCustomResponseSpy.restore();
     });
   });
 });
