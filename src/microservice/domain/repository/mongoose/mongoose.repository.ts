20cfc3cdd1e4a23f1e3e54d8d8226d1b01e2bbf2
Maick Speck <maicksantos05@hotmail.com>
2022-06-23 14:25:39 -0300

    fix sonar qa

--- src/microservice/domain/repository/mongoose/mongoose.repository.ts
+++ src/microservice/domain/repository/mongoose/mongoose.repository.ts
@@ -1,47 +1,19 @@
 import { Logger } from '@nestjs/common';
-import { ClientSession, Model } from 'mongoose';
+import { Model } from 'mongoose';
 
 export abstract class MongooseRepository<Collection, MongooseModel> {
   protected readonly logger: Logger = new Logger(this.constructor.name);
-  private session: ClientSession;
 
   constructor(protected model: Model<MongooseModel>) {}
 
   async create(document: Collection): Promise<void> {
     return new Promise(async (resolve, reject) => {
-      await this.model.create(document, function (err) {
+      this.model.create(document, function (err) {
         if (err) reject(err);
         resolve();
       });
     });
   }
-
-  // private async startSession(): Promise<ClientSession> {
-  //   this.session = await this.connection.startSession();
-  //   return this.session;
-  // }
-
-  // async startTransaction(): Promise<void> {
-  //   if (!this.session || !this.session.inTransaction())
-  //     await this.startSession();
-
-  //   await this.session.startTransaction();
-  //   this.logger.log('Starting transaction...');
-  // }
-
-  // async commit(): Promise<void> {
-  //   if (this.session.inTransaction()) {
-  //     this.logger.log('Commit transaction...');
-  //     await this.session.commitTransaction();
-  //   }
-  // }
-
-  // async rollback(): Promise<void> {
-  //   if (this.session.inTransaction()) {
-  //     this.logger.error('Rollback transaction...');
-  //     await this.session.abortTransaction();
-  //   }
-  // }
 
   buildRegexFilterQuery(objSearch: object = {}) {
     const objSearchRegex = {};
