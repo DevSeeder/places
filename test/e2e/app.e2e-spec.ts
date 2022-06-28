b04f4e12695767f9ee5332ce45e71a689657427a
Maick Speck <maicksantos05@hotmail.com>
2022-06-28 09:42:02 -0300

    e2e new routes

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -5,7 +5,7 @@ import { AppModule } from '../../src/app.module';
 import { NestFactory } from '@nestjs/core';
 import '../../src/microservice/adapter/helper/extensions/exensions.module';
 
-jest.setTimeout(75000);
+jest.setTimeout(400000);
 
 describe('App (e2e) ', () => {
   let app: INestApplication;
@@ -39,6 +39,42 @@ describe('App (e2e) ', () => {
       );
       expect(actual.body).to.be.an('object').that.is.not.empty;
       expect(actual.body.Orleans).to.be.an('array').that.is.not.empty;
+    });
+  });
+
+  describe('City (e2e) ', () => {
+    it('/cities/state/brasil/sc (GET)', async () => {
+      const actual = await request(app.getHttpServer()).get(
+        '/cities/state/brasil/sc'
+      );
+
+      expect(actual.body).to.be.an('array').that.is.not.empty;
+    });
+
+    it('/cities/country/brasil (GET)', async () => {
+      const actual = await request(app.getHttpServer()).get(
+        '/cities/country/brasil'
+      );
+
+      expect(actual.body).to.be.an('array').that.is.not.empty;
+    });
+  });
+
+  describe('State (e2e) ', () => {
+    it('/states/country/brasil (GET)', async () => {
+      const actual = await request(app.getHttpServer()).get(
+        '/states/country/brasil'
+      );
+
+      expect(actual.body).to.be.an('array').that.is.not.empty;
+    });
+  });
+
+  describe('Country (e2e) ', () => {
+    it('/countries/ (GET)', async () => {
+      const actual = await request(app.getHttpServer()).get('/countries/');
+
+      expect(actual.body).to.be.an('array').that.is.not.empty;
     });
   });
 });
