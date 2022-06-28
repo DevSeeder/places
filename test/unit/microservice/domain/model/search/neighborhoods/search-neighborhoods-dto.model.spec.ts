41acf73601ed1f8c185b9af9a74490e8a3c8c3d9
Maick Speck <maicksantos05@hotmail.com>
2022-06-27 21:12:28 -0300

    improve cov tests swagger

--- test/unit/microservice/domain/model/search/neighborhoods/search-neighborhoods-dto.model.spec.ts
+++ test/unit/microservice/domain/model/search/neighborhoods/search-neighborhoods-dto.model.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { SearchNeighborhoodsDTO } from '../../../../../../../src/microservice/domain/model/search/neighborhoods/search-neighborhoods-dto.model';
+import '../../../../../../../src/microservice/adapter/helper/extensions/exensions.module';
 
 describe('SearchNeighborhoodsDTO', () => {
   it('should instance SearchNeighborhoodsDTO and return the object with the correct properties', async () => {
@@ -18,5 +19,15 @@ describe('SearchNeighborhoodsDTO', () => {
     expect(model.state).to.be.equal('sc');
     expect(model.city).to.be.equal('praia grande');
     expect(model.name).to.be.equal('Vila Rosa');
+  });
+
+  it('should instance SearchNeighborhoodsDTOand throws invalid data exception', async () => {
+    const model = new SearchNeighborhoodsDTO('', 'sc', 'praia grande');
+
+    try {
+      model.validateIsAnyEmptyKey();
+    } catch (err) {
+      expect(err.message).to.be.equal(`The property 'Country' cannot be empty`);
+    }
   });
 });
