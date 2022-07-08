ebcbe1e070203844e40fb366c3234c3992ea19ef
Maick Speck <maicksantos05@hotmail.com>
2022-07-08 14:51:38 -0300

    fix search guia mais

--- src/microservice/adapter/helper/extensions/string.extension.ts
+++ src/microservice/adapter/helper/extensions/string.extension.ts
@@ -1,11 +1,16 @@
 declare global {
   interface String {
     capitalize(): string;
+    replaceAll(search: string, replacement: string): string;
   }
 }
 
 String.prototype.capitalize = function () {
   return this.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
+};
+
+String.prototype.replaceAll = function (search: string, replacement: string) {
+  return this.split(search).join(replacement);
 };
 
 export {};
