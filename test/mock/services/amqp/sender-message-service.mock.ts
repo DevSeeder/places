45f7652ea1ad32041b6e13773f31caac27c6500f
Maick Speck <maicksantos05@hotmail.com>
2022-08-02 11:06:37 -0300

    fix critical unit test async seed

---
+++ test/mock/services/amqp/sender-message-service.mock.ts
@@ -0,0 +1,5 @@
+export const mockSenderMessageService = {
+  publishMessage: () => {
+    return;
+  }
+};
---
+++ test/mock/services/logseed/log-seed-service.mock.ts
@@ -0,0 +1,5 @@
+export const mockLogSeedJobService = {
+  logSeedByState: () => {
+    return;
+  }
+};
